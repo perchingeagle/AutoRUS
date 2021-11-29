d0e29116016cc0fc4d77f00a50a2040fd85d8c2b
perchingeagle <perchingeagle@gmail.com>
2021-11-28 22:20:30 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,19 @@ mechacar.df = read.csv(file=mechacar_file, stringsAsFactors=TRUE)
 lm_stat <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar.df)
 
 summary(lm_stat)
+
+suspension_file = paste(file_path, 'Suspension_Coil.csv', sep='/')
+
+suspension.df = read.csv(file=suspension_file, stringsAsFactors=TRUE)
+
+head(suspension.df)
+
+total_summary <- summarize(suspension.df, Mean=mean(PSI), 
+                           Median=median(PSI),
+                           Variance=var(PSI),
+                           SD=sd(PSI))
+
+lot_summary <- suspension.df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), 
+                                                         Median=median(PSI),
+                                                         Variance=var(PSI),
+                                                         SD=sd(PSI))
